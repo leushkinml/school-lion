0285d0c5ff9bde3841c273178283e60f6a7b858d
leush <leushkinml@mail.ru>
2023-01-17 18:23:49 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/schoollion/SchoolLionApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoollion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolLionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogwarts/schoollion/SchoolLionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoollion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
