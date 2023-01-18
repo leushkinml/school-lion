f3c7e0e198ada3d69ceb2a66b5d9dd33bd69b90e
leush <leushkinml@mail.ru>
2023-01-18 23:38:03 +0300

    Commit 3    18.01.2023    23:35 Подключена база данных.
    Commit 2    17.01.2023    18:51

--- src/main/java/ru/hogwarts/schoollion/model/Faculty.java
+++ src/main/java/ru/hogwarts/schoollion/model/Faculty.java
@@ -14,14 +14,14 @@ public class Faculty {
     private String name;
     private String color;
 
-//    public Faculty() {
-//
-//    }
-//    public Faculty(long id, String name, String color) {
-//        this.id = id;
-//        this.name = name;
-//        this.color = color;
-//    }
+    public Faculty() {
+
+    }
+    public Faculty(long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
 
     public long getId() {
         return id;
